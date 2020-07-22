4136da74666b0dbfac918bd2d4942b8941ddbd65
zhangz <china9591@live.com>
2020-07-22 18:13:13 +0800

    add question m11

---
+++ Easy/m11_rolating_min_array_LCOF/rolating_min_array_LCOF.go
@@ -0,0 +1,30 @@
+package question9
+
+/*
+	剑指 Offer 11. 旋转数组的最小数字
+	把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。  
+*/
+
+/*
+	思路: 暴力了，其实二分也行
+	题解: 二分
+*/
+
+func minArray(numbers []int) int {
+
+    if len(numbers) == 1 {
+        return numbers[0]
+    }
+
+    for i := 1; i < len(numbers) - 1; i++ {
+        if numbers[i] < numbers[i - 1] && numbers[i] <= numbers[i + 1] {
+            return numbers[i]
+        }
+    }
+
+    if numbers[len(numbers) - 1] > numbers[0] {
+        return numbers[0]
+    } else {
+        return numbers[len(numbers) - 1]
+    }
+}
